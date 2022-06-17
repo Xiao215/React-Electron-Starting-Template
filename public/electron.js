f4cc60bcdf595189a26f0da49bd6785a85c381c6
Xiao215 <xiaozhang20030215@gmail.com>
2022-06-17 15:35:55 -0400

    commit

--- public/electron.js
+++ public/electron.js
@@ -1,10 +1,17 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
 const path = require("path");
 const isDev = require("electron-is-dev");
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    title: "Converter",
+    width: 1400,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
   });
   mainWindow.loadURL(
     isDev
@@ -14,6 +21,8 @@ function createWindow() {
   if (isDev) {
     mainWindow.webContents.openDevTools();
   }
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 }
 app.whenReady().then(() => {
   createWindow();
@@ -24,3 +33,53 @@ app.whenReady().then(() => {
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
 });
+
+const menuTemplate = [
+  {
+    label: "Converter",
+    submenu: [
+      {
+        label: "About",
+      },
+      {
+        label: "Github",
+        click() {},
+      },
+      {
+        label: "Quit",
+        click() {
+          app.quit();
+        },
+        accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
+      },
+    ],
+  },
+  {
+    label: "Files",
+    submenu: [
+      { label: "Download location" },
+      { label: "Set download location" },
+    ],
+  },
+];
+if (process.platform === "darwin") {
+  menuTemplate.unshift({ label: "" });
+}
+if (process.env.NODE_ENV !== "production") {
+  //production, development, staging, test
+  menuTemplate.push({
+    label: "DEVELOPER",
+    submenu: [
+      { role: "reload" },
+      {
+        label: "Toggle Developer Tools",
+        click(item, focusedWindow) {
+          //item is useless but need to be added
+          focusedWindow.toggleDevTools();
+        },
+        accelerator:
+          process.platform === "darwin" ? "Command+Alt+I" : "Ctrl+Shift+I",
+      },
+    ],
+  });
+}
---
+++ src/DrawerLeft.js
@@ -0,0 +1,183 @@
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import HistoryIcon from "@mui/icons-material/History";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import SvgIcon from "@mui/material/SvgIcon";
+import HomeIcon from "@mui/icons-material/Home";
+const drawerWidth = 240;
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+function BilibiliIcon() {
+  return (
+    <SvgIcon>
+      <path d="M17.813 4.653h.854c1.51.054 2.769.578 3.773 1.574 1.004.995 1.524 2.249 1.56 3.76v7.36c-.036 1.51-.556 2.769-1.56 3.773s-2.262 1.524-3.773 1.56H5.333c-1.51-.036-2.769-.556-3.773-1.56S.036 18.858 0 17.347v-7.36c.036-1.511.556-2.765 1.56-3.76 1.004-.996 2.262-1.52 3.773-1.574h.774l-1.174-1.12a1.234 1.234 0 0 1-.373-.906c0-.356.124-.658.373-.907l.027-.027c.267-.249.573-.373.92-.373.347 0 .653.124.92.373L9.653 4.44c.071.071.134.142.187.213h4.267a.836.836 0 0 1 .16-.213l2.853-2.747c.267-.249.573-.373.92-.373.347 0 .662.151.929.4.267.249.391.551.391.907 0 .355-.124.657-.373.906zM5.333 7.24c-.746.018-1.373.276-1.88.773-.506.498-.769 1.13-.786 1.894v7.52c.017.764.28 1.395.786 1.893.507.498 1.134.756 1.88.773h13.334c.746-.017 1.373-.275 1.88-.773.506-.498.769-1.129.786-1.893v-7.52c-.017-.765-.28-1.396-.786-1.894-.507-.497-1.134-.755-1.88-.773zM8 11.107c.373 0 .684.124.933.373.25.249.383.569.4.96v1.173c-.017.391-.15.711-.4.96-.249.25-.56.374-.933.374s-.684-.125-.933-.374c-.25-.249-.383-.569-.4-.96V12.44c0-.373.129-.689.386-.947.258-.257.574-.386.947-.386zm8 0c.373 0 .684.124.933.373.25.249.383.569.4.96v1.173c-.017.391-.15.711-.4.96-.249.25-.56.374-.933.374s-.684-.125-.933-.374c-.25-.249-.383-.569-.4-.96V12.44c.017-.391.15-.711.4-.96.249-.249.56-.373.933-.373Z" />
+    </SvgIcon>
+  );
+}
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+export default function DrawerLeft(props) {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const [currentPageName, setCurrentPageName] = React.useState("Home");
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            {currentPageName}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {["Home", "Youtube", "Bilibili", "Convert"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton onClick={() => setCurrentPageName(text)}>
+                <ListItemIcon>
+                  {(() => {
+                    if (index === 0) {
+                      return <HomeIcon />;
+                    } else if (index === 1) {
+                      return <YouTubeIcon />;
+                    } else if (index === 2) {
+                      return <BilibiliIcon />;
+                    } else if (index === 3) {
+                      return <VideoLibraryIcon />;
+                    }
+                  })()}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["Account", "History", "Contact Us"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton onClick={() => setCurrentPageName(text)}>
+                <ListItemIcon>
+                  {(() => {
+                    if (index === 0) {
+                      return <AccountCircleIcon />;
+                    } else if (index === 1) {
+                      return <HistoryIcon />;
+                    } else if (index === 2) {
+                      return <MailIcon />;
+                    }
+                  })()}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        {props.children}
+      </Main>
+    </Box>
+  );
+}
